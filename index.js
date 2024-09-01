645aaff58eac9898422b240a1eb992369bdb80e3
Muhammad Fahd Rafique Khan <68250458+fahadkhan7103333@users.noreply.github.com>
2024-09-01 23:00:36 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+import products from './products.js';
+import cart from './cart.js';
+
+
+
+
+
+
+
+let app = document.getElementById('app');
+let temporaryContent = document.getElementById('temporaryContent');
+
+// load layout file
+const loadTemplate = () => {
+    fetch('/template.html')
+    .then(response => response.text())
+    .then(html => {
+        app.innerHTML = html;
+        let contentTab = document.getElementById('contentTab');
+        contentTab.innerHTML = temporaryContent.innerHTML;
+        temporaryContent.innerHTML = null;
+        cart();
+        initApp();
+    })
+}
+loadTemplate();
+const initApp = () => {
+     // load list product
+     let listProductHTML = document.querySelector('.listProduct');
+     listProductHTML.innerHTML = null;
+     
+     products.forEach(product => {
+         let newProduct = document.createElement('div');
+         newProduct.classList.add('item');
+         newProduct.innerHTML = 
+         `<a href="/detail.html?id=${product.id}">
+             <img src="${product.image}">
+         </a>
+         <h2>${product.name}</h2>
+         <div class="price">$${product.price}</div>
+         <button 
+             class="addCart" 
+             data-id='${product.id}'>
+                 Add To Cart
+         </button>
+         <div class="rate">
+    <input type="radio" id="star5" name="rate" value="5" />
+    <label for="star5" title="text">5 stars</label>
+    <input type="radio" id="star4" name="rate" value="4" />
+    <label for="star4" title="text">4 stars</label>
+    <input type="radio" id="star3" name="rate" value="3" />
+    <label for="star3" title="text">3 stars</label>
+    <input type="radio" id="star2" name="rate" value="2" />
+    <label for="star2" title="text">2 stars</label>
+    <input type="radio" id="star1" name="rate" value="1" />
+    <label for="star1" title="text">1 star</label>
+  </div>
+  `;
+         listProductHTML.appendChild(newProduct);
+    });
+}
---
+++ products.js
@@ -0,0 +1,60 @@
+const products = [
+    {
+        "id": 1,
+        "name":" ZMA® JYM Zinc & Magnesium",
+        "price": 200,
+        "image": "images/1.webp",
+        "description": "Help Support Recovery, Strength, and Immune Function"
+    },
+    {
+        "id": 2,
+        "name":" Alpha JYM X Advanced",
+        "price": 250,
+        "image": "images/2.webp",
+        "description": "Alpha JYM X is an advanced testosterone optimizer containing nine ingredients designed to support optimal testosterone levels and promote muscle growth, strength, and energy."
+    },
+    {
+        "id": 3,
+        "name":" Stim-Free Pre JYM Pre-Workout",
+        "price": 290,
+        "image": "images/3.webp",
+        "description": "The high-performance pre-workout formula that changed the sports nutrition industry is now available with no caffeine."
+    },
+    {
+        "id": 4,
+        "name":" High-Performance Pre-Workout",
+        "price": 200,
+        "image": "images/4.webp",
+        "description": "Pre-Workout Powder Powerhouse Packed with Lab and Gym Tested Ingredients to Support Improved Workouts"
+    },
+    {
+        "id": 5,
+        "name":" JYM Men's Tee - Cardinal Heather",
+        "price": 300,
+        "image": "images/5.webp",
+        "description": "When you look good, you feel good. Thanks to the 60%/40% Ring Spun Cotton/Polyester blend, this crew neck tee is soft, comfortable and designed with a flattering athletic fit that is perfect for hard training sessions. No more hitting the gym in a baggy t-shirt"
+    },
+    {
+        "id": 6,
+        "name":" JYM Men's Tee - Cardinal Heather",
+        "price": 200,
+        "image": "images/6.webp",
+        "description": "When you look good, you feel good. Thanks to the 60%/40% Ring Spun Cotton/Polyester blend, this crew neck tee is soft, comfortable and designed with a flattering athletic fit that is perfect for hard training sessions. No more hitting the gym in a baggy t-shirt"
+    },
+    {
+        "id": 7,
+        "name":" JYM Stainless Shaker-Black",
+        "price": 200,
+        "image": "images/7.webp",
+        "description": "The Black JYM-branded Radian Insulated Stainless Steel BlenderBottle will keep your pre-workout, post-workout, and protein shakes as cold as you like them for up to 24 hours and with no annoying condensation. You also get the same convenient features as with other BlenderBottle products – including BlenderBall wire whisk for optimal mixability and a versatile carry loop for your keys or gym bag."
+    },
+    {
+        "id": 8,
+        "name":" Naturally Flavored & Sweetened",
+        "price": 200,
+        "image": "images/8.webp",
+        "description": "Fast-Digesting Carbs to Restore Muscle Glycogen and Enhance Nutrient Uptake"
+    }
+
+];
+export default products;
